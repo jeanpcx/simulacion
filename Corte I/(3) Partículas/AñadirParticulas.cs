4d840ed1a26fa6946c6d183633823d0035891405
jeancpx <61467452+jeancpx@users.noreply.github.com>
2020-05-29 19:22:55 -0500

    Add files via upload

---
+++ Corte I/(3) Partículas/AñadirParticulas.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AñadirParticulas : MonoBehaviour
+{
+
+    public int particleAmount;      //Cantidad de partículas
+    public GameObject myPrefab;
+    float a;
+    float dt = 0.01f;
+    private List<GameObject> lParticle = new List<GameObject>();
+    public void Añadir()
+    {
+        GameObject g = Instantiate(myPrefab, new Vector3(100, 100, 100), Quaternion.identity);
+    }
+}
