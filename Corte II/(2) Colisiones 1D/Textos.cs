242ad86e87868f1785945d0772f7667de3a699a4
jeancpx <61467452+jeancpx@users.noreply.github.com>
2020-05-29 19:32:58 -0500

    Add files via upload

---
+++ Corte II/(2) Colisiones 1D/Textos.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Textos : MonoBehaviour
+{
+    public Text V_1;
+    public Text V_2;
+
+    public Text M_1;
+    public Text M_2;
+
+    public Text C_1;
+    public Text C_2;
+
+    GameObject O1;
+    GameObject O2;
+ 
+    public float MA1;
+    public float MA2;
+    public float CO1;
+    public float CO2;
+    public float VE1;
+    public float VE2;
+   
+    // Update is called once per frame
+    public void enviar()
+    {
+        VE1 = float.Parse(V_1.text);
+        VE2 = float.Parse(V_2.text);
+        MA1 = float.Parse(M_1.text);
+        MA2 = float.Parse(M_2.text);
+        CO1 = float.Parse(C_1.text);
+        CO2 = float.Parse(C_2.text);
+        O1 = GameObject.Find("Esfera -");
+        O2 = GameObject.Find("Esfera +");
+        O1.GetComponent<movimiento>().V.x = VE1;
+        O2.GetComponent<mov_2>().V.x = VE2;
+        
+        O1.GetComponent<movimiento>().MA1 = MA1;
+        O2.GetComponent<mov_2>().MA1 = MA1;
+
+
+        O1.GetComponent<movimiento>().MA2 = MA2;
+        O2.GetComponent<mov_2>().MA2 = MA2;
+
+        O1.GetComponent<movimiento>().CO1 = CO1;
+        O2.GetComponent<mov_2>().CO1 = CO1;
+
+        O1.GetComponent<movimiento>().CO2 = CO2;
+        O2.GetComponent<mov_2>().CO2 = CO2;
+    }
+
+   
+}
---
+++ Corte II/(2) Colisiones 1D/movimiento.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimiento :  MonoBehaviour
+{
+    public float MA1;
+    public float MA2;
+    public float CO1;
+    public float CO2;
+    public Vector3 V;
+    Vector3 V2;
+    public Vector3 P;
+    Vector3 PM1;
+    Vector3 Vx;
+    float e;
+    public float D;
+   // float Masa2;
+    float Radio;
+    float R;
+    float tiempo = 0.01f;
+   public  float contador = 0;
+    GameObject M1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vx.x = 0;
+        Vx.y = 0;
+        Vx.z = 0;
+        Radio = 0.5f;
+        M1 = GameObject.Find("Esfera +");
+        R = 2 * Radio;
+      
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        P = gameObject.GetComponent<Transform>().position;
+        PM1 = M1.GetComponent<Transform>().position;
+        V2 = M1.GetComponent<mov_2>().V;
+        D = Mathf.Abs(P.x - PM1.x);
+
+        if (D <= (R + 0.1f))
+        {
+            contador++;
+        }
+        if(contador==1)
+        {
+            e = (CO1 + CO2) / 2;
+            Vx = V * ((MA1 - e * MA2) / (MA1 + MA2)) + V2 * (((1 + e) * MA2) / (MA1 + MA2));
+        }
+        if(Vx.x ==0)
+        {
+            P = P + V * tiempo;
+        }
+        else
+        {
+            P = P + Vx * tiempo;
+        }
+ 
+        gameObject.GetComponent<Transform>().position = P;
+    }
+}
