d383c133d78d73ca15d1688aa1e61d5e012a8484
jeancpx <61467452+jeancpx@users.noreply.github.com>
2020-05-29 19:20:01 -0500

    Add files via upload

---
+++ Corte I/(2) Planetas/Movi.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movi : MonoBehaviour
+{
+    public GameObject planeta1;
+    public GameObject planeta2;
+
+    float dt;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.fixedDeltaTime = 0.02f;
+        dt = Time.fixedDeltaTime;
+    }
+    Vector3 fuerza(GameObject p1, GameObject p2)
+    {
+        float G =5f;
+        Vector3 R = p1.transform.position - p2.transform.position;
+        float R_magnitud = R.magnitude;
+        Vector3 Rdireccion = R / R_magnitud;
+        float F_magnitud = (G * p2.GetComponent<Planeta>().masaPlaneta * p1.GetComponent<Planeta>().masaPlaneta) / (R_magnitud * R_magnitud);
+        Vector3 F_Vect = -F_magnitud * Rdireccion;
+
+        return F_Vect;
+
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Debug.Log(dt);
+        Vector3 f1 = fuerza(planeta1, planeta2);
+        Vector3 f2 = fuerza(planeta2, planeta1);
+        planeta1.GetComponent<Planeta>().momento = planeta1.GetComponent<Planeta>().momento + f1*dt;
+        planeta2.GetComponent<Planeta>().momento = planeta2.GetComponent<Planeta>().momento + (f2*dt);
+
+        planeta1.GetComponent<Transform>().position = planeta1.GetComponent<Transform>().position + (planeta1.GetComponent<Planeta>().momento / planeta1.GetComponent<Planeta>().masaPlaneta) * dt;
+        planeta2.GetComponent<Transform>().position = planeta2.GetComponent<Transform>().position + (planeta2.GetComponent<Planeta>().momento / planeta2.GetComponent<Planeta>().masaPlaneta) * dt;
+    }
+}
---
+++ Corte I/(2) Planetas/Planeta.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planeta : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector3 momento;
+    public float masaPlaneta;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
